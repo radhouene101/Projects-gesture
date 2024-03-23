8bfdabf8c3a1329c5f4cc3f5e465568b1d8279a9
radhouene101 <radhouene.berrzig@esprit.tn>
2024-03-23 07:34:56 +0100

    all dtos

--- src/main/java/tn/bal/pi/dto/UserDto.java
+++ src/main/java/tn/bal/pi/dto/UserDto.java
@@ -1,9 +1,12 @@
 package tn.bal.pi.dto;
 
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.UniqueElements;
 import tn.bal.pi.entities.User;
 
 @Getter
@@ -11,11 +14,25 @@ import tn.bal.pi.entities.User;
 @AllArgsConstructor
 @Builder
 public class UserDto {
+
+    private Long id;
+    @Email
+    @NotBlank(message = "email should not be blank")
+    @NotNull(message = "email should not be Null")
+    @NotEmpty(message = "email should Not be Empty!")
     private String email;
+    @NotBlank(message = "password should not be blank")
+    @NotNull(message = "password should not be Null")
+    @NotEmpty(message = "password should Not be Empty!")
+    @Size(min = 2 , max = 32)
     private String password;
+    @NotBlank(message = "fullname should not be blank")
+    @NotNull(message = "fullname should not be Null")
+    @NotEmpty(message = "fullname should Not be Empty!")
     private String fullname;
     public static UserDto fromEntity(User user){
         return UserDto.builder()
+                .id(user.getId())
                 .fullname(user.getFullname())
                 .email(user.getEmail())
                 .password(user.getPassword())
@@ -23,6 +40,7 @@ public class UserDto {
     }
     public static User toEntity(UserDto user){
         return User.builder()
+                .id(user.getId())
                 .fullname(user.getFullname())
                 .email(user.getEmail())
                 .password(user.getPassword())
